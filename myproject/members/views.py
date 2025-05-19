aeafa0ce36a9e238710615d9c89420323c7e3a78
sumitraay2022@gmail.com <sumitraay2022@gmail.com>
2025-05-19 20:08:15 +0545

    added About dynamic

--- myproject/members/views.py
+++ myproject/members/views.py
@@ -1,9 +1,11 @@
 # members/views.py
 from django.shortcuts import render
-from .models import Role, Project, ContactInfo
+from .models import Role, Project, ContactInfo, About, Skill
 
 def home(request):
     roles = Role.objects.all()
+    about = About.objects.all()
+    skill = Skill.objects.all()
     project = Project.objects.all()
     contact = ContactInfo.objects.first()
 
@@ -11,6 +13,8 @@ def home(request):
     context = {
         'name': 'Sumit Ray',
         'roles': roles,
+        'about': about,
+        'skill': skill,
         'project': project,
         'contact': contact,
     }
