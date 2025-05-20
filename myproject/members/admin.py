91ac013e858276a9351c1c180ce5ff860f18b613
sumitraay2022@gmail.com <sumitraay2022@gmail.com>
2025-05-20 18:00:22 +0545

    made dynamic form page

--- myproject/members/admin.py
+++ myproject/members/admin.py
@@ -2,10 +2,11 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib import admin
-from .models import Role, Project, ContactInfo,About,Skill
+from .models import Role, Project, ContactInfo,About,Skill,Contact
 
 admin.site.register(Role)
 admin.site.register(About)
 admin.site.register(Skill)
 admin.site.register(Project)
 admin.site.register(ContactInfo)
+admin.site.register(Contact)
